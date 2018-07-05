c581234397679ea1cd169dbcabf234e6c7e154c7
achref-sellami <sallemi.achref@gmail.com>
2018-07-05 23:26:37 +0100

    create the user interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+
+  allumette: number[] = [1, 2, 3];
+  allumettesNumber: number = Math.floor(Math.random() * 10) + 15;
+  allumettesItems: number[] = [];
+
+  constructor(){}
+
+  public ngOnInit(){
+    this.createAllumettesItems();
+  }
+
+  createAllumettesItems(){
+    for(var i = 1; i <= this.allumettesNumber; i++){
+      this.allumettesItems.push(i);
+    }
+    return this.allumettesItems;
+  }
+  
 }
